b6dc3aff8a406251e6185d7532f0cd750694272c
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2023-05-22 18:02:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 from dotenv import load_dotenv
 import os
-from flask_sqlalchemy import SQLAlchemy
-
-# from models import SessionDb
-# import redis
+import redis
 
 load_dotenv()
 
 class ApplicationConfig:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///./db.sqlite"
-
-    SESSION_TYPE = "sqlalchemy"
-    SESSION_SQLALCHEMY_DB_MODEL = "SessionDb"
+    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
+    
+    SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_COOKIE_DOMAIN = ".onrender.com"
+    
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    # REDIS_URL = "redis://red-chibjq3hp8u7g2fcfm6g:6379"
+    # SESSION_REDIS = redis.from_url(REDIS_URL)
