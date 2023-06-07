e9f3560d95fc8d1cf78961b58a2d9ef7a89a1634
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2023-06-07 20:48:29 +0100

    Update models.py

--- models.py
+++ models.py
@@ -40,7 +40,7 @@ class User(db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text, nullable=False)
-    qr_code = db.Column(db.String(255))
+    qr_code = db.Column(db.Text)
     paid = db.Column(db.Boolean, default=False)
     payment_reference = db.Column(db.String(255), default=None)
     first_name = db.Column(db.String(255))
